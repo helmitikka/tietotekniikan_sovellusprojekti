2436d61e654b1fd215c563778cb21af723f03523
Lomri <varkkij@gmail.com>
2024-09-16 17:58:07 +0300

    Luotu initializeTimer funktio

--- SpedenSpelit.ino
+++ SpedenSpelit.ino
@@ -2,6 +2,8 @@
 #include "buttons.h"
 #include "leds.h"
 #include "SpedenSpelit.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 // Use these 2 volatile variables for communicating between
 // loop() function and interrupt handlers
@@ -37,6 +39,21 @@ void loop()
 void initializeTimer(void)
 {
 	// see requirements for the function from SpedenSpelit.h
+  // Hmm nämä menee Arduinon Verifystä läpi mutta VS Code ilmeisesti ei ymmärrä:
+  // Timer asetukset
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // Aseta vertailuarvo (OCR1A) 0.1 sekunnin välein
+  OCR1A = 1562;
+
+  // Aseta CTC-tila ja prescaleri
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+
+  // Ota käyttöön Timer1:n keskeytys
+  TIMSK1 |= (1 << OCIE1A);
 }
 ISR(TIMER1_COMPA_vect)
 {
