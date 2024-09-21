2f1f39802123905aab78c921026a5aebf8bcccd3
juhoggez <jho.honkanen@gmail.com>
2024-09-21 20:42:47 +0300

    leds.h extern volatile

--- SpedenSpelit/leds.h
+++ SpedenSpelit/leds.h
@@ -2,6 +2,9 @@
 #define LEDS_H
 #include <arduino.h>
 
+extern volatile bool gameRunning;
+extern volatile int currentScore;
+
 /*
   initializeLeds() subroutine intializes analog pins A2,A3,A4,A5
   to be used as outputs. Speden Spelit leds are connected to those
